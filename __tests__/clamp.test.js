74860ba257d9867029625fd5cdbeec5b7b964aca
Iita Selkama <iita.selkama@tuni.fi>
2025-12-07 23:12:49 +0200

    fix clamp test 3 toBe from 56 to 0

--- __tests__/clamp.test.js
+++ __tests__/clamp.test.js
@@ -9,5 +9,5 @@ test('Give parameters 56, 0, 100, gives 56', () => {
 });
 
 test('Give parameters -56, 0, 100, gives 0', () => {
-  expect(clamp(-56, 0, 100)).toBe(56);
+  expect(clamp(-56, 0, 100)).toBe(0);
 });
