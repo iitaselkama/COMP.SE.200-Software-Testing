abeb7b0ddb0c47e86a21e86fe6bede121fcac0c1
Iita Selkama <iita.selkama@tuni.fi>
2025-12-07 23:05:53 +0200

    add test cases to clamp.test.js

---
+++ __tests__/clamp.test.js
@@ -0,0 +1,13 @@
+import clamp from '../src/clamp.js';
+
+test('Give parameters 348, 0, 100, gives 100', () => {
+  expect(clamp(348, 0, 100)).toBe(100);
+});
+
+test('Give parameters 56, 0, 100, gives 56', () => {
+  expect(clamp(56, 0, 100)).toBe(56);
+});
+
+test('Give parameters -56, 0, 100, gives 0', () => {
+  expect(clamp(-56, 0, 100)).toBe(56);
+});
