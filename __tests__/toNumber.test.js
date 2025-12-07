dafb212bd00bac21c4fa255d015593705373fc40
Iita Selkama <iita.selkama@tuni.fi>
2025-12-07 21:44:08 +0200

    add test cases to toNumber.test.js

---
+++ __tests__/toNumber.test.js
@@ -0,0 +1,33 @@
+import toNumber from '../src/toNumber.js';
+
+test('parameter is 5.263, gives 5.263', () => {
+  expect(toNumber(5.263)).toBe(5.263);
+});
+
+test('parameter is -4832, gives -4832', () => {
+  expect(toNumber(-4832)).toBe(-4832);
+});
+
+test('parameter is 0, gives 0', () => {
+  expect(toNumber(0)).toBe(0);
+});
+
+test('parameter is "123", gives 123', () => {
+  expect(toNumber('123')).toBe(123);
+});
+
+test('parameter is "abc", gives NAN', () => {
+  expect(toNumber("abc")).toBe(NAN);
+});
+
+test('parameter is new Number(58), gives 58', () => {
+  expect(toNumber(new Number(58))).toBe(58);
+});
+
+test('parameter is new String("abc"), gives NAN', () => {
+  expect(toNumber(new String("abc"))).toBe(NAN);
+});
+
+test('parameter is new String("123"), gives 123', () => {
+  expect(toNumber(new String("123"))).toBe(123);
+});
