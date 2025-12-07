f37dcff0a9822413b1c64a414bbfb4acf0ec1f80
Iita Selkama <iita.selkama@tuni.fi>
2025-12-07 23:46:02 +0200

    change erroneous paths, change '404' result to undefined

--- __tests__/get.test.js
+++ __tests__/get.test.js
@@ -19,26 +19,24 @@ test('For testObject give path "id[0].name[1]" and defaultValue, gives "Lockwood
   expect(get(testObject, "id[0].name[1]", defaultValue)).toBe("Lockwood");
 });
 
-test('For testObject give path ["id", "0", "age"], no defaultValue, gives 24', () => {
-  expect(get(testObject, ["id", "0", "age"])).toBe(24);
+test('For testObject give path ["id", "1", "age"], no defaultValue, gives 24', () => {
+  expect(get(testObject, ["id", "1", "age"])).toBe(24);
 });
 
-test('For testObject give path "id[0].age" and defaultValue, gives 24', () => {
-  expect(get(testObject, "id[0].age", defaultValue)).toBe(24);
+test('For testObject give path "id[1].age" and defaultValue, gives 24', () => {
+  expect(get(testObject, "id[1].age", defaultValue)).toBe(24);
 });
 
-//This one is not passing as there is no consideration for a faulty path with no defaultValue established
-test('For testObject give path ["id", "1"], no defaultValue, gives "not found" error code', () => {
-  expect(get(testObject, ["id", "1"])).toBe("404");
+test('For testObject give path ["id", "2"], no defaultValue, gives undefined', () => {
+  expect(get(testObject, ["id", "2"])).toBe(undefined);
 });
 
-test('For testObject give path ["id", "1"] and defaultValue, gives defaultValue', () => {
-  expect(get(testObject, ["id", "1"], defaultValue)).toBe(defaultValue);
+test('For testObject give path "id[2]" and defaultValue, gives defaultValue', () => {
+  expect(get(testObject, "id[2]", defaultValue)).toBe(defaultValue);
 });
 
-//This one is not passing as there is no consideration for a faulty path with no defaultValue established
-test('For testObject give path [], no defaultValue, gives "not found" error code', () => {
-  expect(get(testObject, [])).toBe("404");
+test('For testObject give path [], no defaultValue, gives undefined', () => {
+  expect(get(testObject, [])).toBe(undefined);
 });
 
 test('For testObject give path [] and defaultValue, gives defaultValue', () => {
